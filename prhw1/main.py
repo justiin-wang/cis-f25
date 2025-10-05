786203dc858c1d90eb5e2828beb7bfb927c1d135
aly9o <youalan9@gmail.com>
2025-10-05 17:16:42 -0400

    started test script for Q4

--- prhw1/main.py
+++ prhw1/main.py
@@ -12,29 +12,26 @@ print(tool.name)
 
 d_known, a_known, c_known = parser.parse_calbody("./data/pa1-debug-c-calbody.txt")
 D_frames, A_frames, C_frames = parser.parse_calreadings("./data/pa1-debug-c-calreadings.txt")
+C_expected_pc = [] # k C_expected point cloud
 
-num_frames = len(D_frames)
-C_expected_pc = []  
-
-for k in range(num_frames):
+for k in range(len(D_frames)):
     D_measured = D_frames[k]
     A_measured = A_frames[k]
 
     F_D = tool.point_cloud_registration(d_known, D_measured)
     F_A = tool.point_cloud_registration(a_known, A_measured)
 
-    F_C = np.linalg.inv(F_D) @ F_A
+    F_C = np.linalg.inv(F_D) @ F_A # F_C = F_D^-1 * F_A
 
-    C_expected = (F_C[:3, :3] @ c_known.T + F_C[:3, 3:4]).T
+    C_expected = (F_C[:3, :3] @ c_known.T + F_C[:3, 3:4]).T # Apply the transformation to vector c_known
     C_expected_pc.append(C_expected)
 
 C_expected_pc = np.array(C_expected_pc)
 C_frames = np.array(C_frames)
 
-print("C_expected_pc shape:", C_expected_pc.shape)
-print("C_frames shape:", C_frames.shape)
-print("Plotting shapes - C_expected:", C_expected_pc.shape, "C_frames:", C_frames.shape)
-
 # Plot C_expected vs C_frames
 fig, ax = plotter.plot_data_2(C_expected_pc, C_frames, "C_expected", "C_measured", number_points=False)
 plt.show()
+
+# TODO: write out to txt file
+
---
+++ prhw1/tests/expected_calibration_object_test.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from utils.calibrator import CalibrationTools
+from utils import parse as parser
+from utils import plot as plotter
+
+tool = CalibrationTools("test")
+
+def find_expected_calibration_object(calbody_path, calreadings_path):
+    d_known, a_known, c_known = parser.parse_calbody(calbody_path)
+    D_frames, A_frames, C_frames = parser.parse_calreadings(calreadings_path)
+    C_expected_pc = [] # k C_expected point cloud
+
+    for k in range(len(D_frames)):
+        D_measured = D_frames[k]
+        A_measured = A_frames[k]
+
+        F_D = tool.point_cloud_registration(d_known, D_measured)
+        F_A = tool.point_cloud_registration(a_known, A_measured)
+
+        F_C = np.linalg.inv(F_D) @ F_A # F_C = F_D^-1 * F_A
+
+        C_expected = (F_C[:3, :3] @ c_known.T + F_C[:3, 3:4]).T # Apply the transformation to vector c_known
+        C_expected_pc.append(C_expected)
+
+    C_expected_pc = np.array(C_expected_pc)
+    C_frames = np.array(C_frames)
+
+    return C_expected_pc, C_frames
+
+perfect_expected, perfect_measured = find_expected_calibration_object("./data/pa1-debug-c-calbody.txt", "./data/pa1-debug-c-calreadings.txt")
+fig, ax = plotter.plot_data_2(perfect_expected, perfect_measured, "Perfect Expected", "Perfect Measured", number_points=False)
+# Calculate errrors, should be close to zero
+
+distorted_expected, distorted_measured = find_expected_calibration_object("./data/pa1-debug-a-calbody.txt", "./data/pa1-debug-a-calreadings.txt")
+fig, ax = plotter.plot_data_2(distorted_expected, distorted_measured, "Distorted Expected", "Distorted Measured", number_points=False)
+plt.show()
+# Calculate errors, should be larger due to distortion
